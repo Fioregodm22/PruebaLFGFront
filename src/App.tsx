9a635565e85760180caf0b50061c5b461e24b726
Juan Pablo <a01638951@tec.mx>
2026-04-09 15:03:50 -0600

    Front

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import { RouterProvider } from "react-router";
+import { router } from "./routes";
+import { Wireframe } from './components/Wireframe';
+
+export default function App() {
+  return (
+    <>
+      <Wireframe/>
+    </>
+  );
+}
